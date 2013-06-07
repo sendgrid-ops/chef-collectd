916820d492fa9ee2b6fb890c27e2c3e710ed9ae5
AJ Bourg <aj.bourg@sendgrid.com>
2013-06-07 15:50:24 -0600

    Move to our own repo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default["collectd"]["version"]            = "5.3.0"
 default["collectd"]["dir"]                = "/opt/collectd"
-default["collectd"]["url"]                = "http://collectd.org/files/collectd-5.3.0.tar.gz"
+default["collectd"]["url"]                = "http://repo.sendgrid.net/collectd-5.3.0.tar.gz"
 default["collectd"]["checksum"]           = "cb361deeda00bece54cec2f7a2c368ccea4ac1c4a83388a0dae0435ba8969ee1"
 default["collectd"]["interval"]           = 10
 default["collectd"]["read_threads"]       = 5
