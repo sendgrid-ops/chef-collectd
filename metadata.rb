b707146a8f043cf8e15808a1c4b55352f4bd641d
sgopsbot <operations+hubot@sendgrid.com>
2015-11-20 21:12:42 -0800

    add in version function

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,15 @@
+if File.file?("version_wf")
+  version_var = IO.read(File.join(File.dirname(__FILE__), 'version_wf'))
+else
+  version_var = "0.0.0"
+end
+
 name              "collectd"
 maintainer        "Hector Castro"
 maintainer_email  "hectcastro@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures collectd."
-version           IO.read(File.join(File.dirname(__FILE__), 'version_wf')) || "testing"
+version           version_var
 
 recipe            "collectd", "Installs and configures collectd"
 recipe            "collectd::attribute_driven", "Installs collectd plugins via node attributes"
@@ -20,3 +26,4 @@ depends           'yum-epel', '~> 0.3'
 %w{ amazon centos fedora redhat scientific ubuntu }.each do |os|
   supports os
 end
+
